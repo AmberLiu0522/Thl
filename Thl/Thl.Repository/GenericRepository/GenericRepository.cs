53b34bde967a428f1193d61dac4161e281b86eb2
AmberLiu0522 <amber.liu.nn@gmail.com>
2021-07-11 17:16:43 +1200

    Thl : Implement Repository functions

---
+++ Thl/Thl.Repository/GenericRepository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Thl.EFCore.Context;
+using Thl.Repository.Contract.IGenericRepository;
+
+namespace Thl.Repository.GenericRepository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly MyContext _db = null;
+        protected readonly DbSet<T> DbSet = null;
+
+        public GenericRepository()
+        {
+            var options = new DbContextOptionsBuilder<MyContext>().Options;
+
+            this._db = new MyContext(options);
+            this.DbSet = _db.Set<T>();
+        }
+        public async Task<IEnumerable<T>> GetAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            DbSet.Add(entity);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            DbSet.Attach(entity);
+            _db.Entry(entity).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ Thl/Thl.Repository/ProductRepository/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Thl.EFCore.Models;
+using Thl.Repository.Contract.IRepository;
+using Thl.Repository.GenericRepository;
+
+namespace Thl.Repository.Repository
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        // Using Default Page Size as 50 to avoid custom query param input mega number as page size
+        private const int PAGE_SIZE = 50;
+
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            return this.GetByIdAsync(id);
+        }
+
+        public Task<IEnumerable<Product>> GetProductsByNameAsync(int page, string name)
+        {
+            var products = this._db.Products
+                .Where(p => p.Name.ToLower() == name.ToLower())
+                .Skip((page - 1) * PAGE_SIZE)
+                .Take(PAGE_SIZE)
+                .ToList();
+
+            return (Task<IEnumerable<Product>>)products.AsEnumerable();
+        }
+
+        public Task AddProductAsync(Product product)
+        {
+            return this.AddAsync(product);
+        }
+
+        public Task UpdateProductAsync(Product product)
+        {
+            return this.UpdateAsync(product);
+        }
+    }
+}
