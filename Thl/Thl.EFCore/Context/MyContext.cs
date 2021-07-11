324334dac5b8673e893a25ca272e6eb733577ee2
AmberLiu0522 <amber.liu.nn@gmail.com>
2021-07-11 16:40:50 +1200

    Thl : Add EF Model

---
+++ Thl/Thl.EFCore/Context/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Thl.EFCore.Models;
+
+namespace Thl.EFCore.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
