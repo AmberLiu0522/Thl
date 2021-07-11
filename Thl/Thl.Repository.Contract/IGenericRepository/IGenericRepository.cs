a7595774f767845a6bdae26459ade77d7043fea3
AmberLiu0522 <amber.liu.nn@gmail.com>
2021-07-11 16:53:27 +1200

    Thl : Create Generic and Custom Repository Interfaces

---
+++ Thl/Thl.Repository.Contract/IGenericRepository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Thl.Repository.Contract.IGenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+    }
+}
---
+++ Thl/Thl.Repository.Contract/IRepository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Thl.EFCore.Models;
+using Thl.Repository.Contract.IGenericRepository;
+
+namespace Thl.Repository.Contract.IRepository
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetProductsByNameAsync(int page, string name);
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+    }
+}
